866b72b0bac19201d72d89c70052c5b5aa8d63c5
Christian Schulz <christian.schulz.phone@gmail.com>
2024-01-08 17:48:13 +0100

    .

---
+++ upload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+scp index.html christians84cs@vieclus.taa.univie.ac.at:/var/www/html/
+scp vieclusv1.00.pdf christians84cs@vieclus.taa.univie.ac.at:/var/www/html/
+#scp default.css i10pc107:/srv/www/directories/documents/kahip/
